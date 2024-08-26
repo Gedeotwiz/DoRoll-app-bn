6aad2fc4f590a0f3021e1bb0345f65b93ac4eeac
Gedeotwiz <gedeontwizerimana6@gmail.com>
2024-08-26 11:44:15 +0200

    removing error

---
+++ src/migrations/store.ts
@@ -0,0 +1,6 @@
+// "migration:run": "npm run build && npm run typeorm migration:run -- -d ./dist/config/typeorm.js",
+    // "migration:generate": "npm run build && npm run typeorm -- -d ./dist/config/typeorm.js migration:generate ./src/migrations/$npm_config_name -p=true",
+    // "migration:create": "npm run build && npm run typeorm -- migration:create ./src/migrations/$npm_config_name",
+    // "migration:revert": "npm run build && npm run typeorm -- -d ./dist/config/typeorm.js migration:revert"
+
+    // "typeorm": "npx typeorm",
