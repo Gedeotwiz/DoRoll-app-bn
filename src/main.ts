3d88e9e43707f74e98fef58ad252be28a5a024ba
Gedeotwiz <gedeontwizerimana6@gmail.com>
2024-09-04 17:27:18 +0200

    adding handle cors

--- src/main.ts
+++ src/main.ts
@@ -9,22 +9,28 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
-    credentials: true,
-    allowedHeaders: 'Content-Type, Authorization', 
+    origin: [
+      'http://localhost:3000', 
+      'https://do-roll-task.vercel.app' 
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
+    credentials: true, 
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
+  
   const config = new DocumentBuilder()
     .setTitle('DoRoll API')
     .setDescription('DoRoll application API in NestJS')
-    .addBearerAuth()
+    .addBearerAuth() 
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
     customSiteTitle: 'DoRoll API Documentation',
   });
-  await app.listen(3001);
+
+  await app.listen(3001); 
 }
 
 bootstrap();
